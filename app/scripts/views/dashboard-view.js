81b7208402c781ae01953499b0f74a63422c5f73
Heather Amler <heatheramler@yahoo.com>
2014-07-10 01:43:39 -0400

    Updating map with nearby scavenger hunts

--- app/scripts/views/dashboard-view.js
+++ app/scripts/views/dashboard-view.js
@@ -1,5 +1,5 @@
 'use strict';
-/* global userGeo, ScavengerHunt */
+/* global userGeo, ScavengerHunt, map */
 
 var DashboardView = Parse.View.extend({
 	el: '#view',
@@ -15,22 +15,22 @@ var DashboardView = Parse.View.extend({
 	},
 	queryLocations: function(event){
 		if ($(event.currentTarget).text() === this.sectionName) {
-			console.log(userGeo.latitude, userGeo.longitude);
+			// console.log(userGeo.latitude, userGeo.longitude);
 			var point = new Parse.GeoPoint({latitude: userGeo.latitude, longitude: userGeo.longitude});
 			var query = new Parse.Query(ScavengerHunt);
 			// query.near('geolocation', point);
 			query.withinMiles('geolocation', point, 30);
 			query.find({
 				success: function(results) {
-					console.log(results);
-
+					results.forEach(function(hunt) {
+						map.addMarker(1, hunt.attributes.geolocation._latitude, hunt.attributes.geolocation._longitude);
+					});
+					// update list of scavenger hunts	
 				},
 				error: function(error) {
 					alert('Error: ' + error.code + ' ' + error.message);
 				}
 			});
-			// when you get hunts results update map
-			// update list of scavenger hunts
 		}
 	}
 });
