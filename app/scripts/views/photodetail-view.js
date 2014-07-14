656d4c466130d3819e37d537d26b6ff89e5235b1
Heather Amler <heatheramler@yahoo.com>
2014-07-13 23:44:30 -0400

    Rendering location name to the Photo detail view

--- app/scripts/views/photodetail-view.js
+++ app/scripts/views/photodetail-view.js
@@ -1,4 +1,5 @@
 'use strict';
+/* global map */
 
 var PhotoDetailView = Parse.View.extend({
 	el: '#view',
@@ -23,45 +24,25 @@ var PhotoDetailView = Parse.View.extend({
 			error: function(object, error) {
 			// The object was not retrieved successfully.
 			// error is a Parse.Error with an error code and description.
-				alert('There was an error retrieving you photo');
+				alert('There was an error retrieving your photo');
 			}
 		});
 	},
 	findPhotoLocation: function(photoModel) {
-		console.log(photoModel);
 		var photoLocation = photoModel.get('location');
 		photoLocation.fetch({
-			success: function(local) {
-				var title = local.get('name');
-				console.log(local);
-				console.log(title);
+			success: function(photolocationModel) {
+				console.log(photolocationModel);
+				var templateMethod = _.template($('#fetched-location-template').text());
+				var rendered = templateMethod(photolocationModel);
+				$('.photodetail-location-name').append(rendered);
+				map.addMarker(1, photolocationModel.attributes.geolocation._latitude, photolocationModel.attributes.geolocation._longitude);
+			},
+			error: function(object, error) {
+			// The object was not retrieved successfully.
+			// error is a Parse.Error with an error code and description.
+				alert('There was an error.');
 			}
 		});
-
-		// var query = relation.query();
-		// query.find({
-		// 	success: function(results) {
-		// 		console.log(results);
-		// 	},
-
-		// 	error: function(error) {
-		// 		console.log('error on rendering locations');
-		// 	}
-		// });
-		// var photoLocation = photoModel.attributes.location.id;
-		// var query = new Parse.Query(Location);
-		// query.equalTo('objectId', photoLocation);
-		// query.get({
-		// 	success: function(results) {
-		// 		console.log(results);
-		// 		results.forEach(function(location){
-		// 			console.log(location);
-		// 		});
-		// 	},
-		// 	error: function(error) {
-		// 		alert('Error: ' + error.code + ' ' + error.message);
-		// 	}
-		// });
-
 	}
 });
