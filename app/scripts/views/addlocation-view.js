e01d447e069acc1f3371fcdde673eab7ef102959
Heather Amler <heatheramler@yahoo.com>
2014-08-06 00:16:40 -0400

    Scavengerhunt query populating drop down menu

--- app/scripts/views/addlocation-view.js
+++ app/scripts/views/addlocation-view.js
@@ -5,11 +5,31 @@ var AddLocationView = Parse.View.extend({
 	template: _.template($('#addlocation-view-template').text()),
 	sectionName: '',
 	events: {
-		'userGeoLocated h2' : 'queryLocations'
+		// 'userGeoLocated h2' : 'queryLocations'
 	},
-	render: function () {
+	render: function() {
 		this.$el.html(this.template);
 		this.sectionName = this.$el.find('h2').text();
+		this.queryScavengerHunts();
 		return this;
+	},
+	queryScavengerHunts: function() {
+		var that = this;
+		var templateMethod = _.template($('#addlocation-menu-template').text());
+		var ScavengerHunt = Parse.Object.extend('ScavengerHunt');
+		var query = new Parse.Query(ScavengerHunt);
+		query.ascending('name');
+		query.find({
+			success: function(results) {
+				results.forEach(function(safari){
+					console.log(safari);
+					$('#safari').append(templateMethod(safari));
+				});
+			},
+			error: function(error) {
+				alert('There was a problem ', error);
+			}
+
+		});
 	}
 });
